41e4bce46f63208bee7969d3fa11bec7b7a986d7
Mahammad <vfvtll@gmail.com>
2020-07-04 17:14:56 +0400

    Blogs controller

--- Aquahach/Controllers/BlogsController.cs
+++ Aquahach/Controllers/BlogsController.cs
@@ -10,16 +10,38 @@ namespace Aquahach.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class BlogsController : ControllerBase
     {
-        // GET api/values
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                AquahackContext result = new AquahackContext();
+                var data = result.Blogs.Take(10);
+                var response = new
+                {
+                    data = data.ToList()
+                };
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                var response = new
+                {
+                    error = e.StackTrace
+                };
+                return StatusCode(512, response);
+            }
+        }
+
         [HttpGet("{id}")]
         public ActionResult Get(int id)
         {
             try
             {
                 AquahackContext result = new AquahackContext();
-                var data = result.Users.Where(c => c.Id == id);
+                var data = result.Blogs.Where(c => c.Id == id);
                 var response = new
                 {
                     data = data.ToList()
@@ -39,19 +61,19 @@ namespace Aquahach.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] User data)
+        public void Post([FromBody] Blog data)
         {
             AquahackContext db = new AquahackContext();
-            db.Users.Add(data);
+            db.Blogs.Add(data);
             db.SaveChanges();
         }
         [HttpPatch]
-        public void Patch(User data)
+        public void Patch(Blog data)
         {
 
             AquahackContext db = new AquahackContext();
-            User dataAttach = new User { Id = data.Id };
-            db.Users.Attach(dataAttach); /// track your stub model
+            Blog dataAttach = new Blog { Id = data.Id };
+            db.Blogs.Attach(dataAttach); /// track your stub model
             db.Entry(dataAttach).CurrentValues.SetValues(data); /// reflection
 
             db.SaveChanges();
@@ -62,8 +84,8 @@ namespace Aquahach.Controllers
         public void Delete(int id)
         {
             AquahackContext db = new AquahackContext();
-            var data = db.Users.Where(o => o.Id == id).First();
-            db.Users.Remove(data);
+            var data = db.Blogs.Where(o => o.Id == id).First();
+            db.Blogs.Remove(data);
         }
     }
 }
