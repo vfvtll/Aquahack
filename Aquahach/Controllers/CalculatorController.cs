0557a342ddb325b816b6fce81f6d160d59795fcf
Mahammad <vfvtll@gmail.com>
2020-07-05 10:28:39 +0400

    Calculater

---
+++ Aquahach/Controllers/CalculatorController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Aquahach.EFDB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aquahach.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        
+        [HttpGet]
+        public ActionResult Get([FromBody]CalculatorItem data)
+        {
+
+            try
+            {
+                string url = "https://waterfootprint.org/calculator-extended";
+                using (var webClient = new WebClient())
+                {
+                    var pars = new NameValueCollection();
+                    pars.Add("Country_ID", data.Country_ID.ToString());
+                    pars.Add("cereal", data.cereal.ToString());
+                    pars.Add("meat", data.meat.ToString());
+                    pars.Add("income", data.income.ToString());
+                    pars.Add("bathnumber", data.bathnumber.ToString());
+                    pars.Add("carweek", data.carweek.ToString());
+                    pars.Add("coffee", data.coffee.ToString());
+                    pars.Add("diary", data.diary.ToString());
+                    pars.Add("dishminute", data.dishminute.ToString());
+                    pars.Add("dishnumber", data.dishnumber.ToString());
+                    pars.Add("dishweek", data.dishweek.ToString());
+                    pars.Add("egg", data.egg.ToString());
+                    pars.Add("fat", data.fat.ToString());
+                    pars.Add("fruit", data.fruit.ToString());
+                    pars.Add("gardenminute", data.gardenminute.ToString());
+                    pars.Add("gardenweek", data.gardenweek.ToString());
+                    pars.Add("laundryload", data.laundryload.ToString());
+                    pars.Add("poolcapacity", data.poolcapacity.ToString());
+                    pars.Add("poolnumber", data.poolnumber.ToString());
+                    pars.Add("rinsingminute", data.rinsingminute.ToString());
+                    pars.Add("root", data.root.ToString());
+                    pars.Add("showerminute", data.showerminute.ToString());
+                    pars.Add("showernumber", data.showernumber.ToString());
+                    pars.Add("sugar", data.sugar.ToString());
+                    pars.Add("tap1", data.tap1.ToString());
+                    pars.Add("tea", data.tea.ToString());
+                    pars.Add("vegetable", data.vegetable.ToString());
+                    var response = webClient.UploadValues(url, pars);
+                    string str = Encoding.UTF8.GetString(response);
+                    var startIndex = str.IndexOf("InstanceBeginEditable");
+                    str = str.Substring(startIndex);
+                    startIndex = str.IndexOf("<br>");
+                    for (int i = 0; i < 4; i++)
+                    {
+                        str = str.Substring(startIndex + 4);
+                        startIndex = str.IndexOf("<br>");
+                    }
+
+                    startIndex = str.IndexOf("</font>");
+                    str = str.Substring(0, startIndex);
+                    var answer = new
+                    {
+                        total = str
+                    };
+                    return Ok(answer);
+                }
+
+            }
+            catch (Exception e)
+            {
+                return StatusCode(512);
+            }
+        }
+    }
+}
---
+++ Aquahach/EFDB/CalculatorItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aquahach.EFDB
+{
+    public class CalculatorItem
+    {
+        public int? Country_ID { get; set; }
+        public int? cereal { get; set; }
+        public int? meat { get; set; }
+        public int? diary { get; set; }
+        public int? egg { get; set; }
+        public int? fat { get; set; }
+        public int? sugar { get; set; }
+        public int? vegetable { get; set; }
+        public int? fruit { get; set; }
+        public int? root { get; set; }
+        public int? coffee { get; set; }
+        public int? tea { get; set; }
+        public int? showernumber { get; set; }
+        public int? showerminute { get; set; }
+        public int? bathnumber { get; set; }
+        public int? tap1 { get; set; }
+        public int? laundryload { get; set; }
+        public int? dishnumber { get; set; }
+        public int? dishminute { get; set; }
+        public int? dishweek { get; set; }
+        public int? carweek { get; set; }
+        public int? gardenweek { get; set; }
+        public int? gardenminute { get; set; }
+        public int? rinsingminute { get; set; }
+        public int? poolcapacity { get; set; }
+        public int? poolnumber { get; set; }
+        public int? income { get; set; }
+    }
+}
